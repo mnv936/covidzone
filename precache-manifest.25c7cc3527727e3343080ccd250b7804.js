566b8a6cc692296bafeda737093f9bc7e04ccc23
mnv936 <manav.ch.936@gmail.com>
2020-05-09 22:59:08 +0530

    Updates

---
+++ precache-manifest.25c7cc3527727e3343080ccd250b7804.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c79b47887a711fe3ceee58bea56b185",
+    "url": "/covidzone/index.html"
+  },
+  {
+    "revision": "cd91b606e5c129d6951e",
+    "url": "/covidzone/static/css/main.df0b34b4.chunk.css"
+  },
+  {
+    "revision": "4790302ad50a782ab939",
+    "url": "/covidzone/static/js/2.61899f4d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/covidzone/static/js/2.61899f4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd91b606e5c129d6951e",
+    "url": "/covidzone/static/js/main.a07f6840.chunk.js"
+  },
+  {
+    "revision": "98d31a6fe9082aaf5011",
+    "url": "/covidzone/static/js/runtime-main.29979718.js"
+  }
+]);
